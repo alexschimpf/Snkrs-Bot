d51fd7c6c6c3ac8921adc2a0e7deb660b7f70f9b
alfredog1976 <alfredog1976@cox.net>
2020-12-01 20:52:27 -0800

    Workarounds to Failed to click buy button exception and Login Error dialog (#25)
    * Updates to match Nike site
    
    Login fixed as well as shoe size and type selection. Also added new parameters: shoe-type and dont-quit. Also, refresh page if shoe size and type selection fails.
    
    * Updated main.py with suggestions for shoe_type
    
    Removed shoe_type command line variations. Replaced original if Youth/Child check with "if shoe_type in ("Y", "C"):"
    
    * Determine size only vs size + size type
    
    Determine size only vs size + size type by checking text of size available first element. Uses regular expression to determine which code logic to use. Size only uses part of code originally used to determine size.
    
    * Update main.py
    
    * scrollIntoView Buy button
    
    scrollIntoView exception occurs when browser window not big enough and Buy button is not in view. Attempts to scroll next element (class="photo-component") since there were issues trying to scroll Buy button element into view. This has only been tested with Firefox.
    
    * Revert
    
    revert
    
    * Revert "Revert"
    
    This reverts commit d3fb3880339a04a86ea8d776226eae77a95a3705.
    
    * Update main.py
    
    * Update README.md
    
    * Update main.py
    
    * Removed random sleep
    
    Removed random sleep from "retry_login"

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pause
 import argparse
 import logging.config
 import re
+import time
+import random
 from selenium import webdriver
 from dateutil import parser as date_parser
 from selenium.webdriver.common.by import By
@@ -50,12 +52,23 @@ def run(driver, shoe_type, username, password, url, shoe_size, login_time=None, 
         LOGGER.info("Waiting until login time: " + login_time)
         pause.until(date_parser.parse(login_time))
 
+    skip_retry_login = True
     try:
         login(driver=driver, username=username, password=password)
+    except TimeoutException:
+        LOGGER.info("Failed to login due to timeout. Retrying...")
+        skip_retry_login = False
     except Exception as e:
         LOGGER.exception("Failed to login: " + str(e))
         six.reraise(Exception, e, sys.exc_info()[2])
 
+    if skip_retry_login is False:   
+        try:
+            retry_login(driver=driver, username=username, password=password)
+        except Exception as e:
+            LOGGER.exception("Failed to retry login: " + str(e))
+            six.reraise(Exception, e, sys.exc_info()[2])
+            
     if release_time:
         LOGGER.info("Waiting until release time: " + release_time)
         pause.until(date_parser.parse(release_time))
@@ -141,17 +154,58 @@ def login(driver, username, password):
     email_input = driver.find_element_by_xpath("//input[@name='emailAddress']")
     email_input.clear()
     email_input.send_keys(username)
+    
     password_input = driver.find_element_by_xpath("//input[@name='password']")
     password_input.clear()
     password_input.send_keys(password)
 
     LOGGER.info("Logging in")
     driver.find_element_by_xpath("//input[@value='SIGN IN']").click()
-    wait_until_visible(driver=driver, xpath="//a[@data-path='myAccount:greeting']")
-
+    
+    wait_until_visible(driver=driver, xpath="//a[@data-path='myAccount:greeting']", duration=5)
+    
     LOGGER.info("Successfully logged in")
 
+def retry_login(driver, username, password):
+    num_retries_attempted = 0
+    num_retries = 5
+    while True:
+        try:            
+            # Xpath to error dialog button
+            xpath = "/html/body/div[2]/div[3]/div[3]/div/div[2]/input"
+            wait_until_visible(driver=driver, xpath=xpath, duration=5)
+            driver.find_element_by_xpath(xpath).click()
+        
+            password_input = driver.find_element_by_xpath("//input[@name='password']")
+            password_input.clear()
+            password_input.send_keys(password)
 
+            LOGGER.info("Logging in")
+            
+            try:
+                driver.find_element_by_xpath("//input[@value='SIGN IN']").click()
+            except Exception as e:
+                if num_retries_attempted < num_retries:
+                    num_retries_attempted += 1
+                    continue
+                else:
+                    LOGGER.info("Too many login attempts. Please restart app.")
+                    break
+                	            
+            if num_retries_attempted < num_retries:
+                num_retries_attempted += 1
+                continue
+            else:
+                LOGGER.info("Too many login attempts. Please restart app.")
+                break
+        except Exception as e:
+            LOGGER.exception("Error dialog did not load, proceed. Error: " + str(e))
+            break
+
+    wait_until_visible(driver=driver, xpath="//a[@data-path='myAccount:greeting']")
+    
+    LOGGER.info("Successfully logged in")
+    
 def select_shoe_size(driver, shoe_size, shoe_type):
     LOGGER.info("Waiting for size dropdown to appear")
     wait_until_visible(driver, class_name="size-grid-button", duration=10)
@@ -178,9 +232,13 @@ def select_shoe_size(driver, shoe_size, shoe_type):
 
 def click_buy_button(driver):
     xpath = "//button[@data-qa='feed-buy-cta']"
+    class_name = "photo-component"
 
     LOGGER.info("Waiting for buy button to become clickable")
     wait_until_clickable(driver, xpath=xpath, duration=10)
+    
+    element = driver.find_element_by_class_name(class_name)
+    driver.execute_script("arguments[0].scrollIntoView(true);" + "window.scrollBy(0,-100);", element)
 
     LOGGER.info("Clicking buy button")
     driver.find_element_by_xpath(xpath).click()
@@ -285,6 +343,7 @@ if __name__ == "__main__":
         driver = webdriver.Chrome(executable_path=executable_path, chrome_options=options)
     else:
         raise Exception("Specified web browser not supported, only Firefox and Chrome are supported at this point")
+
     shoe_type = args.shoe_type
         
     run(driver=driver, shoe_type=shoe_type, username=args.username, password=args.password, url=args.url, shoe_size=args.shoe_size,
